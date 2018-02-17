69acf5b919d71cb8251073a91d9ef0784ae9eb75
n3td0g <n3td0g@mail.ru>
2018-02-17 19:37:48 +0300

    New dungeon generator

--- Source/DsRoguelike/Private/Dungeon/DungeonGenerator2.cpp
+++ Source/DsRoguelike/Private/Dungeon/DungeonGenerator2.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "DungeonGenerator2.h"
+#include "DungeonLeaf.h"
 
-
+#pragma optimize("", off)
 // Sets default values
 ADungeonGenerator2::ADungeonGenerator2()
 {
@@ -14,19 +15,45 @@ ADungeonGenerator2::ADungeonGenerator2()
 // Called when the game starts or when spawned
 void ADungeonGenerator2::BeginPlay()
 {
-	Super::BeginPlay();
-	
+	Initialize();
+	Super::BeginPlay();	
 }
 
 void ADungeonGenerator2::Initialize()
 {
+	CleanupDungeon();
+
 	HalfDungeonWidth = FMath::FloorToInt(DungeonWidth * 0.5f);
 	HalfDungeonHeight = FMath::FloorToInt(DungeonHeight * 0.5f);
+
+	DungeonWidth = HalfDungeonWidth * 2;
+	DungeonHeight = HalfDungeonHeight * 2;
+
+	LastRow = DungeonWidth - 1;
+	LastCol = DungeonHeight - 1;
+
+	DungeonData = new int32*[DungeonWidth];
+	for (int32 Row = 0; Row < DungeonWidth; ++Row) {
+		DungeonData[Row] = new int32[DungeonHeight];
+		for (int32 Col = 0; Col < DungeonHeight; ++Col) {
+			DungeonData[Row][Col] = DUNGEON_NOTHING;
+		}
+	}
+
+	for (int32 Row = 0; Row < DungeonWidth; ++Row) {
+		DungeonData[Row][0] = DUNGEON_BLOCKED;
+		DungeonData[Row][LastCol] = DUNGEON_BLOCKED;
+	}
+
+	for (int32 Col = 0; Col < DungeonHeight; ++Col) {
+		DungeonData[0][Col] = DUNGEON_BLOCKED;
+		DungeonData[LastRow][Col] = DUNGEON_BLOCKED;
+	}
 }
 
 void ADungeonGenerator2::GenerateDungeon()
 {
-
+	PrintDungeon();
 }
 
 void ADungeonGenerator2::CleanupDungeon()
@@ -37,12 +64,42 @@ void ADungeonGenerator2::CleanupDungeon()
 		}
 		delete[] DungeonData;
 	}
+	CleanupLeaf(RootLeaf);
+	RootLeaf = nullptr;
 }
 
-// Called every frame
-void ADungeonGenerator2::Tick(float DeltaTime)
+void ADungeonGenerator2::CleanupLeaf(FDungeonLeaf* Leaf)
 {
-	Super::Tick(DeltaTime);
+	if (!Leaf) {
+		return;
+	}
+	CleanupLeaf(Leaf->LeftLeaf);
+	CleanupLeaf(Leaf->RightLeaf);
+	delete Leaf;
+}
+
+void ADungeonGenerator2::ProcessLeaf(FDungeonLeaf* Leaf, int32 Iteration)
+{
 
 }
 
+void ADungeonGenerator2::PrintDungeon()
+{
+	int32 Index = 0;
+	for (int32 J = DungeonHeight - 1; J >= 0; --J) {
+		FString DungeonRow;
+		for (int32 I = 0; I < DungeonWidth; ++I) {
+			int32 Data = DungeonData[I][J];
+			int8 C = ' ';
+
+			if (Data & DUNGEON_BLOCKED) {
+				C = '+';
+			}
+			DungeonRow.Append(FString::Printf(TEXT("%c"), C));
+			Index++;
+		}
+		UE_LOG(LogTemp, Log, TEXT("%s"), *DungeonRow);
+	}
+}
+
+#pragma optimize("", on)
--- Source/DsRoguelike/Public/Dungeon/DungeonGenerator2.h
+++ Source/DsRoguelike/Public/Dungeon/DungeonGenerator2.h
@@ -6,6 +6,26 @@
 #include "GameFramework/Actor.h"
 #include "DungeonGenerator2.generated.h"
 
+#define DUNGEON_NOTHING		0x00000000
+#define DUNGEON_BLOCKED		0x00000001
+
+struct FDungeonLeaf;
+
+USTRUCT(BlueprintType)
+struct FPoint2D
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 X;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 Y;
+
+	FPoint2D() : X(0), Y(0) {}
+
+	FPoint2D(int32 NewX, int32 NewY) : X(NewX), Y(NewY) {}
+};
 UCLASS()
 class DSROGUELIKE_API ADungeonGenerator2 : public AActor
 {
@@ -15,9 +35,9 @@ public:
 	// Sets default values for this actor's properties
 	ADungeonGenerator2();
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+	UFUNCTION(BlueprintCallable)
+	void GenerateDungeon();
 
 protected:
 	// Called when the game starts or when spawned
@@ -26,8 +46,6 @@ protected:
 	UFUNCTION()
 	void Initialize();
 
-	UFUNCTION(BlueprintCallable)
-	void GenerateDungeon();
 protected:
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Options", meta = (ClampMin = 10))
 	int32 DungeonWidth = 50;
@@ -36,14 +54,30 @@ protected:
 	int32 DungeonHeight = 50;
 
 private:
+	//Cleanup
 	UFUNCTION()
 	void CleanupDungeon();
+	void CleanupLeaf(FDungeonLeaf* Leaf);
+
+	//BSP
+	void ProcessLeaf(FDungeonLeaf* Leaf, int32 Iteration);	
+
+	//Printing
+	UFUNCTION()
+	void PrintDungeon();
 
 private:
 	UPROPERTY(Transient)
 	int32 HalfDungeonWidth;
 	UPROPERTY(Transient)
 	int32 HalfDungeonHeight;
+
+	UPROPERTY(Transient)
+	int32 LastRow;
+	UPROPERTY(Transient)
+	int32 LastCol;
+
+	FDungeonLeaf* RootLeaf = nullptr;
 
 	int32** DungeonData;	
 };
