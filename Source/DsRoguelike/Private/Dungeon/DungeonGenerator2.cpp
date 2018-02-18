f2786d65f598ee59b863ffb1368126464f45de54
n3td0g <n3td0g@mail.ru>
2018-02-19 00:51:13 +0300

    New generator

--- Source/DsRoguelike/Private/Dungeon/DungeonGenerator2.cpp
+++ Source/DsRoguelike/Private/Dungeon/DungeonGenerator2.cpp
@@ -3,6 +3,11 @@
 #include "DungeonGenerator2.h"
 #include "DungeonLeaf.h"
 
+const FPoint2D FPoint2D::Up = FPoint2D(0, 1);
+const FPoint2D FPoint2D::Down = FPoint2D(0, -1);
+const FPoint2D FPoint2D::Left = FPoint2D(-1, 0);
+const FPoint2D FPoint2D::Right = FPoint2D(1, 0);
+
 #pragma optimize("", off)
 // Sets default values
 ADungeonGenerator2::ADungeonGenerator2()
@@ -15,7 +20,6 @@ ADungeonGenerator2::ADungeonGenerator2()
 // Called when the game starts or when spawned
 void ADungeonGenerator2::BeginPlay()
 {
-	Initialize();
 	Super::BeginPlay();	
 }
 
@@ -25,6 +29,12 @@ void ADungeonGenerator2::Initialize()
 
 	HalfDungeonWidth = FMath::FloorToInt(DungeonWidth * 0.5f);
 	HalfDungeonHeight = FMath::FloorToInt(DungeonHeight * 0.5f);
+
+	HalfMinRoomSize = 1 + MinRoomSize / 2;
+	HalfMaxRoomSize = (MaxRoomSize + 1) / 2;
+	if (HalfMaxRoomSize < HalfMinRoomSize) {
+		Swap(HalfMinRoomSize, HalfMaxRoomSize);
+	}
 
 	DungeonWidth = HalfDungeonWidth * 2;
 	DungeonHeight = HalfDungeonHeight * 2;
@@ -53,6 +63,18 @@ void ADungeonGenerator2::Initialize()
 
 void ADungeonGenerator2::GenerateDungeon()
 {
+	Initialize();
+	RootLeaf = new FDungeonLeaf(0, 0, HalfDungeonWidth - 1, HalfDungeonHeight - 1);
+
+	//Rooms
+	ProcessLeaf(RootLeaf, Iterations);
+	CreateRoom(RootLeaf);
+	CleanupLeaf(RootLeaf);
+
+	//Doors
+	OpenRooms();
+
+	//Print
 	PrintDungeon();
 }
 
@@ -66,6 +88,7 @@ void ADungeonGenerator2::CleanupDungeon()
 	}
 	CleanupLeaf(RootLeaf);
 	RootLeaf = nullptr;
+	Rooms.Empty();
 }
 
 void ADungeonGenerator2::CleanupLeaf(FDungeonLeaf* Leaf)
@@ -80,7 +103,187 @@ void ADungeonGenerator2::CleanupLeaf(FDungeonLeaf* Leaf)
 
 void ADungeonGenerator2::ProcessLeaf(FDungeonLeaf* Leaf, int32 Iteration)
 {
+	if (!Leaf) {
+		return;
+	}
 
+	if (Leaf->RightLeaf != nullptr || Leaf->LeftLeaf != nullptr) {
+		return;
+	}
+
+	if (!Iteration) {
+		return;
+	}
+
+	bool SplitHorizontal = FMath::RandBool();
+	float Ratio = (Leaf->Height * 1.0f) / Leaf->Width;
+	if (Ratio < MinLeafRatio) {
+		SplitHorizontal = true;
+	}
+	else {
+		if ((1.0f / Ratio) < MinLeafRatio) {
+			SplitHorizontal = false;
+		}
+	}
+
+	if (!SplitLeaf(Leaf, SplitHorizontal)) {
+		if (!SplitLeaf(Leaf, !SplitHorizontal)) {
+			return;
+		}
+	}
+
+	ProcessLeaf(Leaf->RightLeaf, Iteration - 1);
+	ProcessLeaf(Leaf->LeftLeaf, Iteration - 1);
+}
+
+bool ADungeonGenerator2::SplitLeaf(FDungeonLeaf* Leaf, bool SplitHorizontal)
+{
+	if (!Leaf) {
+		return false;
+	}
+
+	if (SplitHorizontal) {
+		int32 MinWidth = FMath::Max(int32(Leaf->Height * MinLeafRatio), HalfMinRoomSize);
+		if (Leaf->Width < MinWidth * 2) {
+			UE_LOG(LogTemp, Log, TEXT("Leaf width is too small -> Width: %d, Height: %d"), Leaf->Width, Leaf->Height);
+			return false;
+		}
+
+		float SplitX = MinWidth + FMath::RandRange(0, Leaf->Width - MinWidth * 2);
+
+		Leaf->LeftLeaf = new FDungeonLeaf(Leaf->X, Leaf->Y, SplitX, Leaf->Height);
+		Leaf->RightLeaf = new FDungeonLeaf(Leaf->X + SplitX, Leaf->Y, Leaf->Width - SplitX, Leaf->Height);
+
+		UE_LOG(LogTemp, Log, TEXT("SplitVertical -> Width: %d, Height: %d, X: %d, Y: %d"), Leaf->Width, Leaf->Height, Leaf->X, Leaf->Y);
+	}
+	else {
+		int32 MinHeight = FMath::Max(int32(Leaf->Width * MinLeafRatio), HalfMinRoomSize);
+		if (Leaf->Height < MinHeight * 2) {
+			UE_LOG(LogTemp, Log, TEXT("Leaf height is too small -> Width: %d, Height: %d"), Leaf->Width, Leaf->Height);
+			return false;
+		}
+
+		float SplitY = MinHeight + FMath::RandRange(0, Leaf->Height - MinHeight * 2);
+
+		Leaf->LeftLeaf = new FDungeonLeaf(Leaf->X, Leaf->Y, Leaf->Width, SplitY);
+		Leaf->RightLeaf = new FDungeonLeaf(Leaf->X, Leaf->Y + SplitY, Leaf->Width, Leaf->Height - SplitY);
+
+		UE_LOG(LogTemp, Log, TEXT("SplitHorizontal -> Width: %d, Height: %d, X: %d, Y: %d"), Leaf->Width, Leaf->Height, Leaf->X, Leaf->Y); 
+	}
+
+	return true;
+}
+
+void ADungeonGenerator2::CreateRoom(const FDungeonLeaf* Leaf)
+{
+	if (!Leaf) {
+		return;
+	}
+
+	if (Leaf->RightLeaf != nullptr || Leaf->LeftLeaf != nullptr) {
+		CreateRoom(Leaf->RightLeaf);
+		CreateRoom(Leaf->LeftLeaf);
+		return;
+	}
+
+	int32 MaxRoomWidth = FMath::Min(Leaf->Width, HalfMaxRoomSize);
+	int32 MaxRoomHeight = FMath::Min(Leaf->Height, HalfMaxRoomSize);
+
+	int32 Width(0);
+	int32 Height(0);	
+
+	if (MaxRoomWidth > MaxRoomHeight) {
+		Height = FMath::RandRange(HalfMinRoomSize, MaxRoomHeight);
+		MaxRoomWidth = FMath::Min(MaxRoomWidth, (int32)(Height / MinRoomRatio));
+		int32 MinRoomWidth = FMath::Max(HalfMinRoomSize, (int32)(Height * MinRoomRatio));
+		if (MaxRoomWidth < MinRoomWidth) {
+			Swap(MinRoomWidth, MaxRoomWidth);
+		}
+		Width = FMath::RandRange(MinRoomWidth, MaxRoomWidth);
+	}
+	else {
+		Width = FMath::RandRange(HalfMinRoomSize, MaxRoomWidth);
+		MaxRoomHeight = FMath::Min(MaxRoomHeight, (int32)(Width / MinRoomRatio));
+		int32 MinRoomHeight = FMath::Max(HalfMinRoomSize, (int32)(Width * MinRoomRatio));
+		if (MaxRoomHeight < MinRoomHeight) {
+			Swap(MinRoomHeight, MaxRoomHeight);
+		}
+		Height = FMath::RandRange(MinRoomHeight, MaxRoomHeight);
+	}
+
+	int32 Row = Leaf->X + FMath::RandRange(0, Leaf->Width - Width);
+	int32 Col = Leaf->Y + FMath::RandRange(0, Leaf->Height - Height);
+
+	int32 RoomX = Row * 2 + 1;
+	int32 RoomY = Col * 2 + 1;
+	int32 RoomRight = (Row + Width) * 2 - 1;
+	int32 RoomTop = (Col + Height) * 2 - 1;
+
+	for (int32 R = RoomX; R <= RoomRight; ++R) {
+		for (int32 C = RoomY; C <= RoomTop; ++C) {
+			int32& Data = DungeonData[R][C];
+			Data |= DUNGEON_ROOM;
+		}
+	}
+
+	Rooms.Push(FRoomDungeon(RoomX, RoomY, (RoomRight - RoomX) + 1, (RoomTop - RoomY) + 1));
+}
+
+void ADungeonGenerator2::OpenRooms()
+{
+	for (auto& Room : Rooms) {
+		OpenRoom(Room);
+	}
+}
+
+void ADungeonGenerator2::OpenRoom(FRoomDungeon& Room)
+{
+	TArray<FPoint2D> DoorPlaces;
+	int32 RoomRight = Room.Position.X + Room.Size.X - 1;
+	int32 RoomTop = Room.Position.Y + Room.Size.Y - 1;
+	for (int32 Row = Room.Position.X + 1; Row <= RoomRight; Row += 2) {
+		if (CheckSill(Row, Room.Position.Y, FPoint2D::Down)) {
+			DoorPlaces.Push(FPoint2D(Row, Room.Position.Y));
+		}
+		if (CheckSill(Row, RoomTop, FPoint2D::Up)) {
+			DoorPlaces.Push(FPoint2D(Row, RoomTop));
+		}
+	}
+	for (int32 Col = Room.Position.Y + 1; Col <= RoomTop; Col += 2) {
+		if (CheckSill(Room.Position.X, Col, FPoint2D::Left)) {
+			DoorPlaces.Push(FPoint2D(Room.Position.X, Col));
+		}
+		if (CheckSill(RoomRight, Col, FPoint2D::Right)) {
+			DoorPlaces.Push(FPoint2D(RoomRight, Col));
+		}
+	}
+
+	for (auto& Door : DoorPlaces) {
+		DungeonData[Door.X][Door.Y] |= ROOM_DOOR;
+	}
+}
+
+bool ADungeonGenerator2::CheckSill(int32 Row, int32 Col, const FPoint2D CheckDirection)
+{
+	if (Row < 3 || Col < 3 || Row >= DungeonWidth - 3 || Col >= DungeonHeight - 3) {
+		return false;
+	}
+
+	int32 DoorR = Row + CheckDirection.X;
+	int32 DoorC = Col + CheckDirection.Y;
+
+	if (DungeonData[DoorR][DoorC] & DOOR_BLOCKED) {
+		return false;
+	}
+
+	int32 OpenR = DoorR + CheckDirection.X;
+	int32 OpenC = DoorC + CheckDirection.Y;
+
+	if (DungeonData[OpenR][OpenC] & DUNGEON_BLOCKED) {
+		return false;
+	}
+
+	return true;
 }
 
 void ADungeonGenerator2::PrintDungeon()
@@ -94,6 +297,15 @@ void ADungeonGenerator2::PrintDungeon()
 
 			if (Data & DUNGEON_BLOCKED) {
 				C = '+';
+			}
+			else if (Data & ROOM_DOOR) {
+				C = 'D';
+			}
+			else if (Data & DUNGEON_ROOM) {
+				C = 'H';
+			}
+			else if (Data & ROOM_PERIMETER) {
+				C = 'X';
 			}
 			DungeonRow.Append(FString::Printf(TEXT("%c"), C));
 			Index++;
--- Source/DsRoguelike/Public/Dungeon/DungeonGenerator2.h
+++ Source/DsRoguelike/Public/Dungeon/DungeonGenerator2.h
@@ -8,13 +8,21 @@
 
 #define DUNGEON_NOTHING		0x00000000
 #define DUNGEON_BLOCKED		0x00000001
+#define DUNGEON_ROOM		0x00000002
+#define DUNGEON_CORRIDOR	0x00000004
+
+#define ROOM_PERIMETER		0x00000008
+#define ROOM_ENTRANCE		0x00000010
+#define ROOM_DOOR			0x00000020
+
+#define DOOR_BLOCKED		(ROOM_DOOR | DUNGEON_BLOCKED)
 
 struct FDungeonLeaf;
 
 USTRUCT(BlueprintType)
 struct FPoint2D
 {
-	GENERATED_BODY()
+	GENERATED_BODY()	
 
 	UPROPERTY(BlueprintReadOnly)
 	int32 X;
@@ -25,7 +33,30 @@ struct FPoint2D
 	FPoint2D() : X(0), Y(0) {}
 
 	FPoint2D(int32 NewX, int32 NewY) : X(NewX), Y(NewY) {}
+
+	static const FPoint2D Up;
+	static const FPoint2D Down;
+	static const FPoint2D Left;
+	static const FPoint2D Right;
 };
+
+USTRUCT(BlueprintType)
+struct FRoomDungeon
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadOnly)
+	FPoint2D Size;
+
+	UPROPERTY(BlueprintReadOnly)
+	FPoint2D Position;
+
+	FRoomDungeon(int32 RoomX, int32 RoomY, int32 RoomWidth, int32 RoomHheight) :
+		Position(RoomX, RoomY), Size(RoomWidth, RoomHheight) {}
+
+	FRoomDungeon() {}
+};
+
 UCLASS()
 class DSROGUELIKE_API ADungeonGenerator2 : public AActor
 {
@@ -53,6 +84,26 @@ protected:
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Options", meta = (ClampMin = 10))
 	int32 DungeonHeight = 50;
 
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Options", meta = (ClampMin = 1))
+	int32 Iterations = 4;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Options")
+	float MinLeafRatio = 0.5f;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Options|Rooms", meta = (ClampMin = 1))
+	int32 MinRoomSize = 4;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Options|Rooms", meta = (ClampMin = 1))
+	int32 MaxRoomSize = 8;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Options|Rooms")
+	float MinRoomRatio = 0.6f;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Options|Rooms", meta = (ClampMin = 1))
+	int32 MaxDoorsCount = 4;
+
+	UPROPERTY(BlueprintReadOnly)
+	TArray<FRoomDungeon> Rooms;
 private:
 	//Cleanup
 	UFUNCTION()
@@ -60,7 +111,14 @@ private:
 	void CleanupLeaf(FDungeonLeaf* Leaf);
 
 	//BSP
-	void ProcessLeaf(FDungeonLeaf* Leaf, int32 Iteration);	
+	void ProcessLeaf(FDungeonLeaf* Leaf, int32 Iteration);
+	bool SplitLeaf(FDungeonLeaf* Leaf, bool SplitHorizontal);
+
+	//Rooms
+	void CreateRoom(const FDungeonLeaf* Leaf);
+	void OpenRooms();
+	void OpenRoom(FRoomDungeon& Room);
+	bool CheckSill(int32 Row, int32 Col, const FPoint2D CheckDirection);
 
 	//Printing
 	UFUNCTION()
@@ -73,9 +131,16 @@ private:
 	int32 HalfDungeonHeight;
 
 	UPROPERTY(Transient)
+	int32 HalfMinRoomSize;
+	UPROPERTY(Transient)
+	int32 HalfMaxRoomSize;
+
+	UPROPERTY(Transient)
 	int32 LastRow;
 	UPROPERTY(Transient)
 	int32 LastCol;
+
+	
 
 	FDungeonLeaf* RootLeaf = nullptr;
 
